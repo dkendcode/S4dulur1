9536a1171c5e496612b718eba08bf60dc4b24b33
dkendcode <deka.permana42@gmail.com>
2019-07-10 10:20:08 +0700

    first commit

--- include/config.php
+++ include/config.php
@@ -2,4 +2,3 @@
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<S4dulur.Net','mikhmon>|>aWNlbA==');
 
-$data['dkendcode'] = array ('1'=>'dkendcode!131634115846.ip-dynamic.com','dkendcode@|@admin','dkendcode#|#nGSdbKWaa2U=','dkendcode%S4dulur.Net','dkendcode^S4dulur.Net','dkendcode&Rp','dkendcode*10','dkendcode(1','dkendcode)0','dkendcode=0','dkendcode@!@disable');
